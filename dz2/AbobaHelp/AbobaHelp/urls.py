c6394afb2a041f11a7e3e34ffd4beceae9d326c1
zeritonik <zaq31337@gmail.com>
2024-11-01 06:28:31 +0300

    dz2 finished

---
+++ dz2/AbobaHelp/AbobaHelp/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for AbobaHelp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('tag/<tag>', views.found_questions, name="tag_questions"),
+    path('question/<int:qid>', views.question_page, name="question_page"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('ask/', views.ask, name="ask"),
+
+    path('admin/', admin.site.urls),
+]
